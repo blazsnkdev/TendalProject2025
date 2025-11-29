2fe1f192cc80fe7622449b986212794717f94b5e
blaz snk <blasasto0914@gmail.com>
2025-11-29 17:32:55 -0500

    preparando para cambiar el nombre de mi rama

---
+++ src/TendalProject.Entities/Entidades/CuentaCliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TendalProject.Entities.Entidades
+{
+    public class CuentaCliente
+    {
+        public int ClienteId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Cliente Cliente { get; set; }
+    }
+}
---
+++ src/TendalProject.Entities/Entidades/Usuario.cs
@@ -0,0 +1,19 @@
+﻿namespace TendalProject.Entities.Entidades
+{
+    public class Usuario
+    {
+        public Guid UsuarioId { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public int MyProperty { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public int? TrabajadorId { get; set; }
+        public Trabajador? Trabajador { get; set; }
+        public int? ClienteId { get; set; }
+        public Cliente? Cliente { get; set; }
+        public bool Activo { get; set; }
+        public DateTime? UltimaConexion { get; set; }
+        public int IntentosFallidos { get; set; }//TODO: che esto es para el futuro para hacer un conteo de intentos fallidos de conexion
+    }
+}
+
