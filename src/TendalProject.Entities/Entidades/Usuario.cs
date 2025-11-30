0331ac5850e501d18566061506aacb4ec1af7d89
blaz snk <blasasto0914@gmail.com>
2025-11-29 20:18:32 -0500

    primera migracion ef

--- src/TendalProject.Entities/Entidades/Usuario.cs
+++ src/TendalProject.Entities/Entidades/Usuario.cs
@@ -3,17 +3,14 @@
     public class Usuario
     {
         public Guid UsuarioId { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public int MyProperty { get; set; }
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; }=null!;
         public DateTime FechaCreacion { get; set; }
-        public int? TrabajadorId { get; set; }
-        public Trabajador? Trabajador { get; set; }
-        public int? ClienteId { get; set; }
-        public Cliente? Cliente { get; set; }
         public bool Activo { get; set; }
         public DateTime? UltimaConexion { get; set; }
         public int IntentosFallidos { get; set; }//TODO: che esto es para el futuro para hacer un conteo de intentos fallidos de conexion
+        public ICollection<UsuarioRol> UsuariosRoles { get; set; } = new List<UsuarioRol>();
+        public Cliente? Cliente { get; set; }
     }
 }
 
---
+++ src/TendalProject.Entities/Entidades/UsuarioRol.cs
@@ -0,0 +1,10 @@
+﻿namespace TendalProject.Entities.Entidades
+{
+    public class UsuarioRol
+    {
+        public Guid UsuarioId { get; set; }
+        public Usuario Usuario { get; set; } = null!;
+        public Guid RolId { get; set; }
+        public Rol Rol { get; set; } = null!;
+    }
+}
