eb1a1a35777cf4695653096530a2260b27ebb484
blaz snk <blasasto0914@gmail.com>
2025-12-01 20:36:15 -0500

    migración de las clases a sql - articulo, categoria, proveedor

---
+++ src/TendalProject.Entities/Entidades/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using TendalProject.Entities.Enum;
+
+namespace TendalProject.Entities.Entidades
+{
+    public class Categoria
+    {
+        public Guid CategoriaId { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string? Descripcion { get; set; }
+        public EstadoCategoria Estado { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public ICollection<Articulo> Articulos { get; set; } = new List<Articulo>();
+    }
+}
---
+++ src/TendalProject.Entities/Enum/EstadoArticulo.cs
@@ -0,0 +1,8 @@
+﻿namespace TendalProject.Entities.Enum
+{
+    public enum EstadoArticulo
+    {
+        Inactivo,
+        Activo
+    }
+}
