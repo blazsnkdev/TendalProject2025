7cfcc6a1fd545a759c9eccf71f02f2311c760eb5
blaz snk <blasasto0914@gmail.com>
2025-12-02 23:44:26 -0500

    agregúe la funcionalidad del mantenimiento de proveedores

---
+++ src/TendalProject.Business/DTOs/Requests/Proveedor/ActualizarProveedorRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace TendalProject.Business.DTOs.Requests.Proveedor
+{
+    public record ActualizarProveedorRequest
+    (
+        Guid ProveedorId,
+        string Nombre,
+        string RazonSocial,
+        string Ruc,
+        string Contacto,
+        string Telefono,
+        string Email,
+        string Direccion
+        );
+}
---
+++ src/TendalProject.Business/DTOs/Requests/Proveedor/RegistrarProveedorRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace TendalProject.Business.DTOs.Requests.Proveedor
+{
+    public record RegistrarProveedorRequest
+    (
+        string Nombre,
+        string RazonSocial,
+        string Ruc,
+        string Contacto,
+        string Telefono,
+        string Email,
+        string Direccion
+        );
+}
