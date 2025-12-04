d75e5141ea800ee82991c490fa7ecbfc9c15affa
blaz snk <blasasto0914@gmail.com>
2025-12-03 23:14:59 -0500

    agrgúe el listado de articulos

---
+++ src/TendalProject.Business/DTOs/Responses/Articulo/ListarArticulosResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace TendalProject.Business.DTOs.Responses.Articulo
+{
+    public record ListarArticulosResponse
+    (
+        Guid ArticuloId,
+        string Codigo,
+        string Nombre,
+        string NombreCategoria,
+        int Stock,
+        string Estado,
+        bool Destacado,
+        int CantidadVentas
+    );
+}
