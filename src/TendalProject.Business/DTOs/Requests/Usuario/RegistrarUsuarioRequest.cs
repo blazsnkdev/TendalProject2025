809bc87d38f0ba23031ad0197339c599b5bc7554
blaz snk <blasasto0914@gmail.com>
2025-12-05 01:55:48 -0500

    add: registro de usuarios

---
+++ src/TendalProject.Business/DTOs/Requests/Usuario/RegistrarUsuarioRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TendalProject.Business.DTOs.Requests.Usuario
+{
+    public record RegistrarUsuarioRequest
+    (
+        string Email,
+        string Password,
+        string ConfirmarPassword
+    );
+}
