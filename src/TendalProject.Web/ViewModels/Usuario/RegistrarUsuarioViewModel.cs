c6fa34d6b46be0f007962c63bf1552ec2132e225
blaz snk <blasasto0914@gmail.com>
2025-12-05 01:55:48 -0500

    add: registro de usuarios

---
+++ src/TendalProject.Web/ViewModels/Usuario/RegistrarUsuarioViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TendalProject.Web.ViewModels.Usuario
+{
+    public class RegistrarUsuarioViewModel
+    {
+        [Display(Name = "Correo electronico"),
+            Required(ErrorMessage ="El correo electronico es obligatorio")]
+        public string Email { get; set; } = string.Empty;
+
+        [Display(Name = "Contraseña"),
+            Required(ErrorMessage = "La contraseña es obligatoria"),
+            StringLength(100, ErrorMessage = "La contraseña debe tener al menos {6} caracteres.",
+                MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Confirmar Contraseña"),
+            Required(ErrorMessage = "La confirmación de la contraseña es obligatoria"),
+            StringLength(100, ErrorMessage = "La confirmación de la contraseña debe tener al menos {6} caracteres.",
+                MinimumLength = 6)]
+        public string ConfirmarPassword { get; set; } = string.Empty;
+    }
+}
