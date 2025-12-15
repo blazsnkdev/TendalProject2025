23a07d1d99b6d20e0e90e4541c3aa6b19fa7f03b
blaz snk <blasasto0914@gmail.com>
2025-12-15 17:17:10 -0500

    add: perfil y actualizar contraseña

---
+++ src/TendalProject.Business/DTOs/Requests/Cliente/ActualizarPasswordClienteRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace TendalProject.Business.DTOs.Requests.Cliente
+{
+    public record ActualizarPasswordClienteRequest
+    (
+        Guid ClienteId,
+        string PasswordActual,
+        string NuevoPassword,
+        string ConfirmarPassword
+    );
+}
